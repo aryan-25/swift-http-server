0cc93ae9d9acdcfc9b7e77782de2927826644606
Aryan Shah <aryan_shah@apple.com>
2026-09-09 13:13:07 +0100

    Remove unnecessary change

--- Sources/NIOHTTPServer/Configuration/NIOHTTPServerConfiguration.swift
+++ Sources/NIOHTTPServer/Configuration/NIOHTTPServerConfiguration.swift
@@ -51,13 +51,6 @@ public struct NIOHTTPServerConfiguration: Sendable {
         public static func hostAndPort(host: String, port: Int) -> Self {
             Self(backing: .hostAndPort(host: host, port: port))
         }
-
-        var description: String {
-            switch self.backing {
-            case .hostAndPort(let host, let port):
-                return "\(host):\(port)"
-            }
-        }
     }
 
     /// Configuration for transport security settings.
