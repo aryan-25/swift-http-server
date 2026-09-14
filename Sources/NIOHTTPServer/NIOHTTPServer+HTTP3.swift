11d7bcfefc811a4e9d492fbcbb9caab919c69ac2
Aryan Shah <aryan_shah@apple.com>
2026-09-14 14:25:59 +0100

    Fix argument label

--- Sources/NIOHTTPServer/NIOHTTPServer+HTTP3.swift
+++ Sources/NIOHTTPServer/NIOHTTPServer+HTTP3.swift
@@ -65,7 +65,7 @@ extension NIOHTTPServer {
             httpVersion: .http3,
             remoteAddress: nil,
             localAddress: nil,
-            peerCertificateChainFuture: nil
+            peerCertificateChain: nil
         )
 
         let connection = Connection(
